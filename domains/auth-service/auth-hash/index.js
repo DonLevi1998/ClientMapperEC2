813e9783fa87792a16a0589632dc0212c92a2281
DonLevi1998 <msnarvaez@uce.edu.ec>
2025-07-24 14:57:24 -0500

    test 10

--- domains/auth-service/auth-hash/index.js
+++ domains/auth-service/auth-hash/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const { hashPasswordEndpoint } = require('./controllers/hashController');
 
 const app = express();
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.post('/hash-password', hashPasswordEndpoint);
